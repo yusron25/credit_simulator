c9f969e1b5983bc0ac6a0b1ccd892d13dc97da4b
DESKTOP-A52M65H\user <muhammadyusron17@gmail.com>
2024-06-29 19:13:55 +0700

    inital commit

---
+++ src/main/java/com/testbackend/credit_simulator/CreditSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.testbackend.credit_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testbackend/credit_simulator/CreditSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbackend.credit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
